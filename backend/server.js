cb8ff17b33aa06407c0d32ba496576a85ab565cb
Imesh Gunasekara <imesh@jrcsoftware.com>
2025-03-08 12:49:15 +0800

    api endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+import router from './routes/product.route.js';
 
 dotenv.config();
 
@@ -9,46 +9,7 @@ const app = express();
 
 app.use(express.json()); // middle layer - allows us to accept JSON data in the req.body
 
-app.get('/api/products', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({ success: true, data: products });
-  } catch (error) {
-    console.log('Error in fetching products:', error.message);
-    res.status(500).json({ success: false, message: 'Server Error' });
-  }
-});
-
-app.post('/api/products', async (req, res) => {
-  const product = req.body; // user will send this data
-
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'Please provide all fields' });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error('Error in Create Product:'.error.message);
-    res.status(500).json({ success: false, message: 'Server Error' });
-  }
-});
-
-app.delete('/api/products/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: 'Product deleted' });
-  } catch (error) {
-    res.status(404).json({ success: false, message: 'Product not found' });
-  }
-});
+app.use('/api/products', router);
 
 app.listen(5000, () => {
   connectDB();
