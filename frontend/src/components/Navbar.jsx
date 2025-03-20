a9b2b74ba674b0a3c92e19bf8e6bb24df473c8dc
Imesh Gunasekara <imesh@jrcsoftware.com>
2025-03-21 07:28:37 +0800

    routes and pages

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Flex } from '@chakra-ui/react';
+
+const Navbar = () => {
+  return (
+    <Container maxWidth={'1140px'}>
+      <Flex></Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { Provider } from './components/ui/provider.jsx';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
