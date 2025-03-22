bf515931e2b65a2459b8e04c9fcdbfa76006864b
Imesh Gunasekara <imesh@jrcsoftware.com>
2025-03-22 08:46:09 +0800

    dark mode

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Container, Flex } from '@chakra-ui/react';
+import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { CiSquarePlus } from 'react-icons/ci';
+import { useColorMode } from './ui/color-mode';
+import { IoMdMoon } from 'react-icons/io';
+import { IoMdSunny } from 'react-icons/io';
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Container maxWidth={'1140px'}>
-      <Flex></Flex>
+    <Container maxWidth={'1140px'} px={4}>
+      <Flex
+        h={16}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDirection={{
+          base: 'column',
+          sm: 'row',
+        }}
+      >
+        <Text>
+          <Link to={'/'}>Product Store</Link>
+        </Text>
+        <HStack>
+          <Link to={'/create'}>
+            <Button>
+              <CiSquarePlus />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === 'light' ? <IoMdMoon /> : <IoMdSunny />}
+          </Button>
+        </HStack>
+      </Flex>
     </Container>
   );
 };
