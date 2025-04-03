0b430f31de6204e906f91ca0c6b787f17e6da5d0
Imesh Gunasekara <imesh@jrcsoftware.com>
2025-04-03 15:29:44 +0800

    home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Container, VStack, Text } from '@chakra-ui/react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={'30'}
+          fontWeight={'bold'}
+          textAlign={'center'}
+          bgGradient={'linear(to-r, cyan.400, blue.500)'}
+        >
+          Current Products
+        </Text>
+      </VStack>
+    </Container>
+  );
 };
 
 export default HomePage;
